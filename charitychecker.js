c19e8b8f59347091deebdaf35bf287e3be286098
kpill <kamalapillai@berkeley.edu>
2020-08-13 09:49:48 -0700

    Update charitychecker.js

--- charitychecker.js
+++ charitychecker.js
@@ -3,4 +3,5 @@ const app_key = "af09573c3fec842aad8a44d18ad57e4d";
 window.onload = function() {
   var title = $( "title" ).html();
   var search = JSON.stringify(title);
+  const searchType = "NAME-ONLY";
 }
