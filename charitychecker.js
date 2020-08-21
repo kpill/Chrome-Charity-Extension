2337ec4216d9d777dc855ea7c238e046ca584120
kpill <kamalapillai@berkeley.edu>
2020-08-21 12:09:20 -0700

    insert website title as search value

--- charitychecker.js
+++ charitychecker.js
@@ -3,9 +3,8 @@ window.onload = function() {
 const app_id = "182dd05a";
 const app_key = "af09573c3fec842aad8a44d18ad57e4d";
 const searchType = "NAME-ONLY";
-// var title = $( "title" ).html();
-// var search = JSON.stringify(title);
-var search = 'black lives matter'
+var title = $( "title" ).html();
+var search = JSON.stringify(title);
 var url = "https://api.data.charitynavigator.org/v2/Organizations?app_id=182dd05a&app_key=af09573c3fec842aad8a44d18ad57e4d&search="
 var url = url.concat(search);
 
