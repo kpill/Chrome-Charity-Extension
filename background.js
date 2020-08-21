c78601713ee9f48aa2433a48b5c14c8b0d0246cf
kpill <kamalapillai@berkeley.edu>
2020-08-21 12:16:51 -0700

    setting example website url

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function() {
    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: }, //url of website if charity navigator response has length >= 1
+          pageUrl: {hostEquals: 'blacklivesmatter.com' }, //url of website if charity navigator response has length >= 1
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
