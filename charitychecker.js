be579f68bb1d2f1cc5a265f3172e275556a7d368
kpill <kamalapillai@berkeley.edu>
2020-08-21 15:26:41 -0700

    Update charitychecker.js

--- charitychecker.js
+++ charitychecker.js
@@ -4,7 +4,6 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 	var url = "https://api.data.charitynavigator.org/v2/Organizations?app_id=182dd05a&app_key=af09573c3fec842aad8a44d18ad57e4d&search="
 	var url = url.concat(search);
 	console.log(url)
-});
 
 fetch(url)
 	.then(response => {
@@ -27,5 +26,6 @@ function appendData(data) {
   	}
 }
 
+});
 
 
